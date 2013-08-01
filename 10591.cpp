bc5fcd3387b2af0ab3dfaed2c2f3dcdd32f41cf1
cxwcfea <cxwcfea@163.com>
2013-08-01 15:02:13 +0800

    ac code of 10591

--- 10591.cpp
+++ 10591.cpp
@@ -1,52 +1,27 @@
 #include<cstdio>
 #include<cstring>
 
-#define MAXHASHSIZE 1000003
-#define MAXN 1000000
+#define MAXN 750 
 
-int head[MAXHASHSIZE];
-int next[MAXN];
 int st[MAXN];
 
-int hash(int v) {
-	/*
-	int s = 0;
-	while (v) {
-		s += v%10;
-		v /= 10;
-	}
-	*/
-	return v % MAXHASHSIZE;
-}
-
-bool try_to_insert(int v) {
-	int h = hash(st[v]);
-	int u = head[h];	
-	while (u) {
-		if (st[v] == st[u]) return false;
-		u = next[u];
-	}
-	next[v] = head[h];
-	head[h] = v;
-	return true;
-}
-
-int resolve() {
-	int s, t, p = 1, v;
-	memset(head, 0, sizeof(head));
-	memset(next, 0, sizeof(next));
-	try_to_insert(p);
+int resolve(int v) {
+	int s, t;
+	memset(st, 0, sizeof(st));
 	while (1) {
 		s = 0;
-		v = st[p];
 		while (v) {
 			t = v%10;
 			s += t*t;
 			v /= 10; 
 		}
-		st[++p] = s;
-		if (s == 1) return 1;
-		if (!try_to_insert(p)) return 0;
+		v = s;
+		if (v == 1) return 1;
+		if (st[v]) {
+			return 0;
+		} else {
+			st[v] = 1;
+		}
 	}	
 }
 
@@ -58,8 +33,7 @@ int main() {
 	scanf("%d", &n);
 	for (int i = 0; i < n; ++i) {
 		scanf("%d", &value);
-		st[1] = value;
-		if (resolve()) {
+		if (resolve(value)) {
 			printf("Case #%d: %d is a Happy number.\n", i+1, value);
 		} else {
 			printf("Case #%d: %d is an Unhappy number.\n", i+1, value);
