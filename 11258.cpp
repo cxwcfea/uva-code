d5a7900ca6e90927494c8b7b1ffbd5f8fcc9eb59
cxwcfea <cxwcfea@163.com>
2013-09-16 16:36:28 +0800

    11258 ac code

--- 11258.cpp
+++ 11258.cpp
@@ -8,42 +8,8 @@ using namespace std;
 #define MAXN 203
 
 char digits[MAXN];
-long long d[MAXN][MAXN];
-long long v[MAXN][MAXN];
-
-long long getInt(int i, int j) {
-	long long &ans = v[i][j];
-	if (ans != -1) return ans;
-	char *p = digits+i;
-	char *end = digits+j;
-	ans = 0;
-	while (p <= end) {	
-		ans *= 10;
-		ans += *p-'0';
-		if (ans > INT_MAX) {
-			ans = -2;
-			break;
-		}
-		++p;
-	}
-	return ans;
-}
-
-long long dp(int i, int j) {
-	long long &ans = d[i][j];
-	if (ans != -1) {
-		return ans;
-	}
-	ans = getInt(i, j);
-	if (ans >= 0) return ans;
-	ans = 0;
-	for (int _i = i; _i < j; ++_i) {
-		long long a = dp(i, _i);
-		long long b = dp(_i+1, j);
-		ans = max(ans, a+b); 
-	}
-	return ans;
-}
+long long num[MAXN][MAXN];
+long long d[MAXN];
 
 int main() {
 #ifdef Debug
@@ -53,10 +19,28 @@ int main() {
 	scanf("%d", &N);
 	for (int i = 0; i < N; ++i) {
 		getchar();
-		scanf("%s", digits);
-		memset(d, -1, sizeof(d));
-		memset(v, -1, sizeof(v));
-		printf("%lld\n", dp(0, strlen(digits)-1));
+		scanf("%s", digits+1);
+		memset(num, -1, sizeof(num));
+		memset(d, 0, sizeof(d));
+		int len = strlen(digits+1);
+		for (int l = 1; l <= 10; ++l) {
+			for (int i = 1, j = l; j <= len; ++i, ++j) {
+				printf("i:%d j:%d\n", i, j);
+				num[i][j] = 0;
+				for (int k = i; k <= j; ++k) {
+					num[i][j] = num[i][j]*10 + (digits[k]-'0');
+				}
+				if (num[i][j] > INT_MAX) {
+					num[i][j] = 0;
+				}
+			}
+		}
+		for (int i = 1; i <= len; ++i) {
+			for (int j = 1; j <= 10 && j <= i; ++j) {
+				d[i] = max(d[i], d[i-j]+num[i-j+1][i]);
+			}
+		}
+		printf("%lld\n", d[len]);
 	}
 	return 0;
 }
